fc11354f373da52390c6d81d0a204caee7191729
bion0625 <stxtory@gmail.com>
2025-06-16 11:18:47 +0900

    컨트롤러에 swagger 관련 설정 추가

--- true-self-sim/back/src/main/java/com/example/demo/controller/UserController.java
+++ true-self-sim/back/src/main/java/com/example/demo/controller/UserController.java
@@ -2,13 +2,18 @@ package com.example.demo.controller;
 
 import com.example.demo.model.dto.Response;
 import com.example.demo.model.dto.UsersRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "UserController", description = "User API")
 public class UserController {
+
+    @Operation(summary = "회원가입", description = "회원가입 요청을 처리")
     @PostMapping("/register")
     public ResponseEntity<Response> register(@RequestBody UsersRequest request) {
         return ResponseEntity.ok(new Response(true));
--- true-self-sim/back/src/main/java/com/example/demo/model/dto/UsersRequest.java
+++ true-self-sim/back/src/main/java/com/example/demo/model/dto/UsersRequest.java
@@ -1,7 +1,9 @@
 package com.example.demo.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+@Schema(description = "회원가입 요청 DTO")
 @Data
 public class UsersRequest {
     private String id;
