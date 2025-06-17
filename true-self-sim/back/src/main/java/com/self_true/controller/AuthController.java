fa5b64a6e3c6de226ca10e53b3448b4dac15f044
bion0625 <stxtory@gmail.com>
2025-06-17 14:09:29 +0900

    상태확인 api 추가

--- true-self-sim/back/src/main/java/com/self_true/controller/AuthController.java
+++ true-self-sim/back/src/main/java/com/self_true/controller/AuthController.java
@@ -1,11 +1,14 @@
 package com.self_true.controller;
 
 import com.self_true.model.dto.request.LoginRequest;
+import com.self_true.model.dto.response.Response;
 import com.self_true.model.dto.response.TokenResponse;
 import com.self_true.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,11 @@ public class AuthController {
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
         String token = authService.getTokenByLogin(request);
         return ResponseEntity.ok(new TokenResponse(token));
+    }
+
+    @Operation(summary = "상태확인")
+    @GetMapping("/me")
+    public ResponseEntity<Response> getMyInfo(@AuthenticationPrincipal String memberId) {
+        return ResponseEntity.ok(new Response(true, memberId));
     }
 }
