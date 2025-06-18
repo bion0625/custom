adfdb6840632b3968728de15f87f88820286eac7
bion0625 <stxtory@gmail.com>
2025-06-18 10:09:46 +0900

    오류 클래스 수정

--- true-self-sim/back/src/main/java/com/self_true/service/AuthService.java
+++ true-self-sim/back/src/main/java/com/self_true/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.self_true.service;
 
+import com.self_true.exception.NotFoundMemberException;
 import com.self_true.model.dto.request.LoginRequest;
 import com.self_true.util.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,6 +24,6 @@ public class AuthService {
         return memberService.findById(request.getId())
                 .filter(member -> passwordEncoder.matches(request.getPassword(), member.getPassword()))
                 .map(member -> jwtUtil.generateToken(member.getMemberId(), List.of(member.getRole())))
-                .orElseThrow(() -> new IllegalArgumentException("member not found"));
+                .orElseThrow(() -> new NotFoundMemberException("member not found"));
     }
 }
