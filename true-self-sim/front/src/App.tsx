e45c3d69c72d9d0f099242fac79f098f21b5ab7b
bion0625 <stxtory@gmail.com>
2025-06-18 11:20:16 +0900

    PublicGame 설정 및 배경 이미지 class 설정

--- true-self-sim/front/src/App.tsx
+++ true-self-sim/front/src/App.tsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
+import PublicGame from "./pages/PublicGame.tsx";
 
 function App() {
 
   return (
       <BrowserRouter>
           <Routes>
+              <Route path={"/"} element={<PublicGame/>}/>
               <Route path={"/login"} element={<Login/>}/>
               <Route path={"/register"} element={<Register/>}/>
           </Routes>
---
+++ true-self-sim/front/src/pages/PublicGame.tsx
@@ -0,0 +1,21 @@
+const PublicGame: React.FC = () => {
+    return (
+        <div>
+            <img className="absolute inset-0 w-full h-full object-cover opacity-50" src="background/mountain.jpg" />
+            <div>
+                <div>
+                    <h1></h1>
+                    <div></div>
+                </div>
+                <div>
+                    <h2></h2>
+                    <p></p>
+                    <div></div>
+                </div>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default PublicGame;
