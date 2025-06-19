986da81367ff3fb2527eb33d9ede8793b5e52969
bion0625 <bion0625@gmail.com>
2025-06-19 20:51:06 +0900

    공개 게임 화면에 로그인, 로그아웃 기능 추가

--- true-self-sim/front/src/pages/PublicGame.tsx
+++ true-self-sim/front/src/pages/PublicGame.tsx
@@ -1,7 +1,14 @@
+import {useNavigate} from "react-router-dom";
+import {useContext} from "react";
+import AuthContext from "../context/AuthContext.tsx";
+
 const PublicGame: React.FC = () => {
+    const navigate = useNavigate();
+    const { user, logout, refreshUser} = useContext(AuthContext);
+
     return (
         <div className="relative min-h-screen w-full overflow-hidden bg-black text-white flex flex-col md:flex-row">
-            <img className="absolute inset-0 w-full h-full object-cover opacity-50" src="background/mountain.jpg" />
+            <img className="absolute inset-0 w-full h-full object-cover opacity-50" src="background/mountain.jpg" alt="background"/>
             <div className="relative z-10 flex-1 flex flex-col items-center pt-6 px-4 space-y-6 md:items-center md:px-8 lg:px-16 lg:pr-72">
                 <div className="w-full max-w-xl flex justify-between items-center">
                     <h1 className="text-2xl md:text-3xl font-bold text-indigo-300">
@@ -11,9 +18,24 @@ const PublicGame: React.FC = () => {
                         <button className="text-sm md:text-base text-green-400 hover:text-green-600">
                             관리자 페이지(is_admin)
                         </button>
-                        <button className="text-sm md:text-base text-blue-400 hover:text-blue-600">
-                            로그인
-                        </button>
+                        {!user && (
+                            <button className="text-sm md:text-base text-blue-400 hover:text-blue-600"
+                                    onClick={() => navigate("/login")}
+                            >
+                                로그인
+                            </button>
+                        )}
+                        {user && (
+                            <button className="text-sm md:text-base text-red-500 hover:text-red-600"
+                                    onClick={async () => {
+                                        await logout();
+                                        await refreshUser();
+                                        navigate("/")
+                                    }}
+                            >
+                                로그아웃
+                            </button>
+                        )}
                     </div>
                 </div>
                 <div className="bg-white/10 backdrop-blur-md rounded-xl shadow-lg p-6 w-full max-w-xl md:max-w-2xl">
