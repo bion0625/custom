52b5789c6537b7a439987cf61e273638077e1105
bion0625 <bion0625@gmail.com>
2025-06-19 20:44:57 +0900

    공개 장면 기본 이미지 추가(로딩 이미지)

--- true-self-sim/front/src/pages/PublicAdmin.tsx
+++ true-self-sim/front/src/pages/PublicAdmin.tsx
@@ -31,7 +31,11 @@ const PublicAdmin: React.FC = () => {
 
     const [useCustomImg, setUseCustomImg] = useState(false);
     const backgroundImgs = [
-        "mountain.jpg"
+        "mountain.jpg",
+        'loading-background1.png',
+        'loading-background2.png',
+        'loading-background3.png',
+        'loading-background4.png',
     ]
 
     useEffect(() => {
