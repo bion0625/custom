60462e7994a25330c994c9e4610530d6c0f411a2
이의정 <stxtory@gmail.com>
2025-06-24 15:48:53 +0900

    feat: allow custom or preset backgrounds for nodes

---
+++ true-self-sim/front/src/constants/backgroundImages.ts
@@ -0,0 +1,7 @@
+export const backgroundImgs = [
+    "mountain.jpg",
+    'loading-background1.png',
+    'loading-background2.png',
+    'loading-background3.png',
+    'loading-background4.png',
+];
