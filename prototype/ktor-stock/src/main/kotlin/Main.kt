69092b4ec0b2aa33d53b350aa1a2de7efd5790d8
bion0625 <stxtory@gmail.com>
2025-07-30 18:02:06 +0900

    비동기 호출 로직 설정

--- prototype/ktor-stock/src/main/kotlin/Main.kt
+++ prototype/ktor-stock/src/main/kotlin/Main.kt
@@ -1,15 +1,23 @@
 package com.stock
 
 import com.stock.stockFetcher.StockInfo
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
+    val page = 13
+
     val companies = StockInfo.getCompanyInfo()
-    val filtered = companies.filter {
-        val priceInfo = StockInfo.getPriceInfoByPage(it.code, 1, 13)
-        if (priceInfo.isEmpty()) return@filter false;
-        val todayHigh = priceInfo.first().high
-        priceInfo.all { it.high < todayHigh }
-    }
-    filtered.forEach { println("금일 신고가 종목: ${it.name}") }
+    println("size: ${companies.size}")
+    val filtered = companies.map { company ->
+        async {
+            val priceInfo = StockInfo.getPriceInfoByPage(company.code, 1, page)
+            if (priceInfo.isEmpty()) return@async null
+            val todayHigh = priceInfo.first().high
+            if (priceInfo.all { it.high <= todayHigh }) company else null
+        }
+    }.awaitAll()
+        .filterNotNull()
+    filtered.forEach { println("${page*10}일 기준 신고가 종목: ${it.name}") }
 }
