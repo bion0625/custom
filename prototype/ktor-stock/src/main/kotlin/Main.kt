21043265154851abb4739fe34232aab3366a455b
bion0625 <bion0625@gmail.com>
2025-07-30 21:08:38 +0900

    걸린 시간 확인 로직 추가 및 일 수 수정

--- prototype/ktor-stock/src/main/kotlin/Main.kt
+++ prototype/ktor-stock/src/main/kotlin/Main.kt
@@ -6,18 +6,25 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    val page = 13
+
+    val start = System.currentTimeMillis()
+
+    val page = 25
 
     val companies = StockInfo.getCompanyInfo()
     println("size: ${companies.size}")
-    val filtered = companies.map { company ->
+    val filtered = companies.map {
         async {
-            val priceInfo = StockInfo.getPriceInfoByPage(company.code, 1, page)
+            val priceInfo = StockInfo.getPriceInfoByPage(it.code, 1, page)
             if (priceInfo.isEmpty()) return@async null
             val todayHigh = priceInfo.first().high
-            if (priceInfo.all { it.high <= todayHigh }) company else null
+            if (priceInfo.all { it.high <= todayHigh }) it else null
         }
     }.awaitAll()
         .filterNotNull()
     filtered.forEach { println("${page*10}일 기준 신고가 종목: ${it.name}") }
+
+    val end = System.currentTimeMillis()
+
+    println("전체 걸린 시간: ${end-start}ms")
 }
