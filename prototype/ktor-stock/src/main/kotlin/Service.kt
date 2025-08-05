3febb1093f9587e1330b984de9a490955ae923c7
bion0625 <stxtory@gmail.com>
2025-08-05 10:49:28 +0900

    ktor Service 설정

---
+++ prototype/ktor-stock/src/main/kotlin/Service.kt
@@ -0,0 +1,31 @@
+package com.stock
+
+import com.stock.stockFetcher.StockInfo
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+
+suspend fun getNewHighPriceStock() = coroutineScope {
+    val start = System.currentTimeMillis()
+    val page = 25
+    val companies = StockInfo.getCompanyInfo()
+    val filtered = companies.map {
+        async {
+            val priceInfo = StockInfo.getPriceInfoByPage(it.code, 1, page)
+            if (priceInfo.isEmpty()) return@async null
+            val todayHigh = priceInfo.first().high
+            if (priceInfo.all { it.high <= todayHigh }) it else null
+        }
+    }.awaitAll()
+        .filterNotNull()
+    val end = System.currentTimeMillis()
+
+    """
+        size: ${companies.size}
+                ${page * 10}일 기준 신고가 종목
+                ${"\n" + filtered.joinToString("\n") { "- - - - - - -> ${it.name}" }}
+                
+                전체 걸린 시간: ${(end - start) / 1000.0}s
+    """.trimIndent()
+
+}
