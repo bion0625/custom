a191f988eff114c7b22ce54f2ea3dc56954c9b3e
bion0625 <stxtory@gmail.com>
2025-08-12 09:52:19 +0900

    코틀린 웹플럭스 프로젝트 추가

---
+++ prototype/lietzsche/src/main/kotlin/com/stxtory/lietzsche/LietzscheApplication.kt
@@ -0,0 +1,11 @@
+package com.stxtory.lietzsche
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LietzscheApplication
+
+fun main(args: Array<String>) {
+	runApplication<LietzscheApplication>(*args)
+}
---
+++ prototype/lietzsche/src/test/kotlin/com/stxtory/lietzsche/LietzscheApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stxtory.lietzsche
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LietzscheApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
